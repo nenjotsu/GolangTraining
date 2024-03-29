9aeb55b4de7780dfbcd4bb44cb011109b9f8e65d
nenjotsu <nenjotsu@protonmail.com>
2024-03-29 19:31:31 +0800

    update pointer

--- 08_pointers/01_referencing/main.go
+++ 08_pointers/01_referencing/main.go
@@ -8,12 +8,12 @@ func main() {
 
 	a := 43
 
-	fmt.Println(a)
-	fmt.Println(&a)
+	fmt.Println("a:", a)
+	fmt.Println("&a:", &a)
 
 	var b = &a
 
-	fmt.Println(b)
+	fmt.Println("b:", b)
 
 	// the above code makes b a pointer to the memory address where an int is stored
 	// b is of type "int pointer"
